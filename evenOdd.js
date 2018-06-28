560656f043ca9170e08df2a738f063f60882e3b8
lmarchetc <39964104+lmarchetc@users.noreply.github.com>
2018-06-29 00:26:11 +0300

    Add files via upload

---
+++ evenOdd.js
@@ -0,0 +1,20 @@
+var num = 247655
+var str = num.toString()
+for (i = 0; i<= str.length; i++ ){
+	str[i]
+}
+
+var a = parseInt(str[0]);
+var b = parseInt(str[1]);
+var c = parseInt(str[2]);
+var d = parseInt(str[3]);
+var f = parseInt(str[4]);
+var g = parseInt(str[5])
+
+
+function evenOrodd(n) {
+	if (n % 2 == 0) {
+        even.push(n)} 
+    else {
+    	odd.push(n)}
+}
